341d1979c8c8a882ab25172987bb8d02b766ad47
Lester Hedges <lester.hedges@gmail.com>
2018-11-21 10:29:51 +0000

    Improved data moving script.

--- CatS/BSS/move_data.sh
+++ CatS/BSS/move_data.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# Make the output directory.
+mkdir -p "/mnt/shared/data/$1"
+
 # Move out output files.
-mv *.out /mnt/shared/data/finished
+mv *.out "/mnt/shared/data/$1/"
 
 # Move the output directories.
 for i in $(ls -d */ | grep '^[0-9]');
 do
-    mv $i/* /mnt/shared/data/finished/;
+    mv $i/* "/mnt/shared/data/$1/"
     rm -r $i
 done
