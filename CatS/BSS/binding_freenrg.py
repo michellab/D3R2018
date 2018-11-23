e427c0d4674c5c71ac6d08420e9a1b0f15537c8e
Lester Hedges <lester.hedges@gmail.com>
2018-11-23 12:45:21 +0000

    Use strip command.

--- CatS/BSS/binding_freenrg.py
+++ CatS/BSS/binding_freenrg.py
@@ -40,14 +40,14 @@ mapping = {}
 if os.path.isfile("%s/FESetup_mappings/merge_errors/%s_%s.txt" % (job_dir, num0, num1)):
     with open("%s/FESetup_mappings/merge_errors/%s_%s.txt" % (job_dir, num0, num1), "r") as file:
         for line in file:
-            data = line.rstrip().split()
+            data = line.strip().split()
             mapping[AtomIdx(int(data[0]))] = AtomIdx(int(data[1]))
 
 # Reverse mapping.
 elif os.path.isfile("%s/FESetup_mappings/merge_errors/%s_%s.txt" % (job_dir, num1, num0)):
     with open("%s/FESetup_mappings/merge_errors/%s_%s.txt" % (job_dir, num1, num0), "r") as file:
         for line in file:
-            data = line.rstrip().split()
+            data = line.strip().split()
             # Invert the indices.
             mapping[AtomIdx(int(data[1]))] = AtomIdx(int(data[0]))
 
